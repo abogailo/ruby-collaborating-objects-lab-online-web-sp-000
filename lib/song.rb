fda58eec276490844f4311fd5552669dab8cf70a
Abagail Hudson <abagailanne@gmail.com>
2019-05-28 21:56:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,9 @@ class Song
   end
 
    def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
+    self.an_artist = Artist.find_or_create_by_name(name)
+    #the class method artist 
+    an_artist.add_song(self)
+    #the class method 
   end
 end
